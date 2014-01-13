a64279b3d0ce7973140e0e984e5fb76498f6355d
Axy Mark <axymark@gmail.com>
2014-01-13 19:58:52 +0100

    updated installer, now adds desktop shortcuts

--- src/convert_install.py
+++ src/convert_install.py
@@ -1,5 +1,10 @@
-import shutil
 import os
+import sys
+import shutil
+import win32com.client
+
+shell = win32com.client.Dispatch("WScript.Shell")
+sysHome = os.path.expanduser('~')
 
 folderSrc = "\\\\192.168.0.200\\c\\tools\\convertScripts\\convert\\"
 folderDest = "C:\\convert\\"
@@ -8,4 +13,11 @@ if not os.path.isdir(folderDest):
 shutil.rmtree(folderDest)
 shutil.copytree(folderSrc, folderDest)
 
-print os.listdir(folderDest)
+shortcut1 = shell.CreateShortCut(sysHome + "\\Desktop\\convert_install.lnk")
+shortcut1.Targetpath = "\\\\192.168.0.200\\c\\tools\\convertScripts\\convert_install.exe"
+shortcut1.WorkingDirectory = "\\\\192.168.0.200\\c\\tools\\convertScripts"
+shortcut1.save()
+
+shortcut2 = shell.CreateShortCut(sysHome + "\\Desktop\\convert.lnk")
+shortcut2.Targetpath = "c:\\convert"
+shortcut2.save()
