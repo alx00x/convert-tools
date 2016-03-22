09cc07855f738ced807708374485edd14bf8a481
koaleksa <koaleksa@gmail.com>
2016-03-22 15:54:27 +0100

    preview postfix moved back

--- convert/_mainscript.bat
+++ convert/_mainscript.bat
@@ -25,12 +25,12 @@ for /f "delims=" %%a in ('dir /b /a:-d !extensions!') do (
 
     echo !fileName!
 
-    ffmpeg -y -i %%a -c:v libx264 -preset slow -pix_fmt yuv420p -b:v 1200k -minrate 1200k -maxrate 1200k -bufsize 1200k -pass 1 -an -f mp4 NUL && ffmpeg -i %%a -c:v libx264 -preset slow -pix_fmt yuv420p -b:v 1200k -minrate 1200k -maxrate 1200k -bufsize 1200k -pass 2 -c:a aac -strict -2 -b:a 128k !fileName!.mp4
+    ffmpeg -y -i %%a -c:v libx264 -preset slow -pix_fmt yuv420p -b:v 1200k -minrate 1200k -maxrate 1200k -bufsize 1200k -pass 1 -an -f mp4 NUL && ffmpeg -i %%a -c:v libx264 -preset slow -pix_fmt yuv420p -b:v 1200k -minrate 1200k -maxrate 1200k -bufsize 1200k -pass 2 -c:a aac -strict -2 -b:a 128k !fileName!_preview.mp4
 
     del ffmpeg2pass-0.log
     del ffmpeg2pass-0.log.mbtree
 
-    ffmpeg -y -i %%a -c:v libx264 -preset slow -pix_fmt yuv420p -profile:v baseline -level 3.0 -an !fileName!_ios.mp4
+    ffmpeg -y -i %%a -c:v libx264 -preset slow -pix_fmt yuv420p -profile:v baseline -level 3.0 -an !fileName!.mp4
 
     ffmpeg -y -i %%a -c:v libtheora -qscale:v 8 -an !fileName!.ogv
 
